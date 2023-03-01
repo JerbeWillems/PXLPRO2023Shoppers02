ef079b23cab2d5f2d2684046566e5abb60b8be2f
DieterClaus <Dieter.Claus@student.pxl.be>
2023-03-01 11:06:44 +0100

    adding the MVC-project

---
+++ PXLPRO2023Shoppers02/PXLPRO2023Shoppers02/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PXLPRO2023Shoppers02.Models;
+using System.Diagnostics;
+
+namespace PXLPRO2023Shoppers02.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ PXLPRO2023Shoppers02/PXLPRO2023Shoppers02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PXLPRO2023Shoppers02.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
