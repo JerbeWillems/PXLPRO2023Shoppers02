d6313f3c2e071310f44951925714a99524f6d250
Pieter Kindermans <pieter.kindermans@brightest.be>
2023-05-28 18:44:06 +0100

    Blazor Connection

---
+++ PXLPRO2023Shoppers02/BlazorProducts/Data/ApiDbContextBlazor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PXLPRO2023Shoppers02.Models;
+using PXLPRO2023Shoppers02.ViewModels;
+
+namespace BlazorProducts.Data
+{
+    public class ApiDbContextBlazor : DbContext
+    {
+        public ApiDbContextBlazor(DbContextOptions<ApiDbContextBlazor> options) : base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<OrderLine> OrdersLines { get; set; }
+        public DbSet<UserInfoModel> UserInfo { get; set; }
+
+
+    }
+}
---
+++ PXLPRO2023Shoppers02/BlazorProducts/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorProducts.Data;
+using Microsoft.EntityFrameworkCore;
+using PXLPRO2023Shoppers02.Models;
+
+namespace BlazorProducts.Services
+{
+    public class ProductService
+    {
+        private readonly ApiDbContextBlazor _context;
+
+        public ProductService(ApiDbContextBlazor apiDbContext)
+        {
+            _context = apiDbContext;
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _context.Product.ToListAsync();
+
+        }
+        public async Task<bool> AddNewProduct(Product product)
+        {
+            await _context.Product.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+    }
+}
